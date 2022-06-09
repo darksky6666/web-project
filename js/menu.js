887dcd10eb1dceab4daa8b80a92035470476fbb7
darksky <skynoelwee@gmail.com>
2022-06-09 13:12:30 +0800

    fix delete confirmation

--- js/menu.js
+++ js/menu.js
@@ -37,6 +37,8 @@ function filterCat(catName) {
 function deleteConfirmBox() {
     let deleteConfirm = confirm('Are you sure you want to remove this entry?\nThis process is not reversible.');
     if (deleteConfirm != true) {
-        window.location.href = history.back();
+        window.location.href="../ro_menuList.php";
+        return false;
     }
+    return true;
 }
