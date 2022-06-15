a1ea42dafc1e3e8c76d0a663a79f406942356213
darksky <skynoelwee@gmail.com>
2022-06-15 22:41:43 +0800

    set db config

--- db/db.php
+++ db/db.php
@@ -1,15 +1,12 @@
 <?php
-define("DBHOST", "localhost");
-define("DBUSER", "root");
-define("DBPASS", "123456");
-
-
-$conn = mysqli_connect(DBHOST, DBUSER, DBPASS);
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-mysqli_select_db($conn, "foody") or die(mysqli_error());
-
-//mysqli_close();
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
